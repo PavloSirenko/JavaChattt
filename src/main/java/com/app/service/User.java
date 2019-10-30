7162cd0697ed1998a773cfa3926b5e9200e7bf70
Pavlo Sirenko <psi.webstudio@gmail.com>
2019-10-30 11:19:01 +0200

    add class User

--- src/main/java/com/app/service/User.java
+++ src/main/java/com/app/service/User.java
@@ -1,4 +1,107 @@
 package com.app.service;
 
 public class User {
+    private String nickName;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private char sex;
+    private int age;
+    private String address;
+
+    public User(String nickName, String password, String email) {
+        this.nickName = nickName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(String nickName, String password, String email, String firstName, String lastName, char sex, int age, String address) {
+        this.nickName = nickName;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nickName='" + nickName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
