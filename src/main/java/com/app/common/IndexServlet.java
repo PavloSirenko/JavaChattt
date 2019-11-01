f52f4afc591848a367f2057918d35f1af8e39411
Pavlo Sirenko <psi.webstudio@gmail.com>
2019-11-01 23:28:13 +0200

    code cleaning

--- src/main/java/com/app/common/IndexServlet.java
+++ src/main/java/com/app/common/IndexServlet.java
@@ -1,15 +1,11 @@
 package com.app.common;
 
-import com.app.service.User;
-import com.app.dao.UsersDB;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
--- src/main/java/com/app/service/User.java
+++ src/main/java/com/app/service/User.java
@@ -11,24 +11,6 @@ public class User {
     private final int age;
     private final String address;
 
-//    public User(String nickName, String password, String email) {
-//        this.nickName = nickName;
-//        this.password = password;
-//        this.email = email;
-//    }
-
-//    public User(String nickName, String password, String email, String firstName, String lastName, String sex, int age, String address) {
-//        this.id = id;
-//        this.nickName = nickName;
-//        this.password = password;
-//        this.email = email;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.sex = sex;
-//        this.age = age;
-//        this.address = address;
-//    }
-
     public User(BuilderUser builderUser) {
         this.id = builderUser.id;
         this.nickName = builderUser.nickName;
@@ -76,38 +58,6 @@ public class User {
     public String getAddress() {
         return address;
     }
-
-//    public void setNickName(String nickName) {
-//        this.nickName = nickName;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public void setSex(String sex) {
-//        this.sex = sex;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
 
     public java.lang.String toString() {
         return "User{" +
